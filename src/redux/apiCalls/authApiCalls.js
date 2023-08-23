ddf34fd66c6adf4726d8e37e1b64d1d1cc512225
billal <boudjellbillal@gmail.com>
2023-08-23 22:51:43 +0200

    update message

--- src/redux/apiCalls/authApiCalls.js
+++ src/redux/apiCalls/authApiCalls.js
@@ -2,47 +2,22 @@ import axios from 'axios'
 import { toast } from "react-toastify";
 import { authActions } from '../slice/userSlice';
 //login user
-export function loginUser(user){
-    return async (dispatch,getState) =>{
-        try {
-             const {data} = await axios.post("http://localhost:8000/api/auth/login",user)
-             dispatch(authActions.login(data))
-             localStorage.setItem('userInfo',JSON.stringify(data));
-        } catch (error) {
-            toast.error(error.response.data.message)
-        }
-    } 
-}
 
-//logout user
-export function logoutUser(){
-    return async (dispatch,getState) =>{
-        dispatch(authActions.logout())
-        localStorage.removeItem("userInfo")
-    } 
-}
+
+
 
 //register user
 export function registerUser(user){
     return async (dispatch,getState) =>{
         try {
-             const {data} = await axios.post("http://localhost:5000/api/v1/auth/signup",user)
+             const {data} = await axios.post("https://backend-chat-7n01.onrender.com/api/v1/auth/signup",user)
+             console.log(data,'dada');
              dispatch(authActions.register(data.message))
         } catch (error) {
-            toast.error(error.response.data.message)
+            console.log(error.response.data.msg)
         }
     } 
 }
 
 
-//verified account
-export function verifyEmail(userId,token){
-    return async (dispatch,getState) =>{
-        try {
-             await axios.get(`http://localhost:8000/api/auth/${userId}/verify/${token}`)
-             dispatch(authActions.setIsEmailVerified())
-        } catch (error) {
-            toast.error(error.response.data.message)
-        }
-    } 
-}
+                            
