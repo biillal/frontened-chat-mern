3f01693863448bf914055ec90a201ae26b7b24eb
billal <boudjellbillal@gmail.com>
2023-08-25 02:43:28 +0200

    updapp

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/home/Home'
 import ChatePage from './pages/home/ChatePage'
 import { ToastContainer } from 'react-toastify'
+import { useSelector } from 'react-redux'
 function App() {
-
+  const {user} = useSelector((state)=>state.auth)
   return (
     <>
       <div className='App '>
       <ToastContainer theme='colored' position='top-center' />
         <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
+          <Route index element={user ? <ChatePage/> : <Home/>}/>
                  
-          <Route path='chatPage' element={<ChatePage/>}/>                 
+          <Route path='chatPage' element={user ? <ChatePage/> : <Home/>}/>                 
         </Routes>
         </BrowserRouter>
       </div>
